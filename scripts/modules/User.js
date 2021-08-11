63de7462b581d335bb41ebe3a066354aacd0f3be
christopher-matthews1 <christopher.matthews@jbhunt.com>
2021-08-11 14:53:04 -0400

    challenge-3

--- scripts/modules/User.js
+++ scripts/modules/User.js
@@ -1,4 +1,18 @@
+export {User}
 
+function User(name, gender, age, registered, req, userStatus) {
+    this.userName = name;
+    this.userGender = gender;
+    this.userAge = age;
+    this.userReq = req;
+    this.userCode = gender + age;
+    this.userStatus = null;
+    this.registered = false;
+
+    return {
+        user: User
+    }
+}
 
 
 
