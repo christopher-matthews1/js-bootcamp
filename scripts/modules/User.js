38edcc2c9c42cbc626f1c657f4221ea9b8ac8fab
christopher-matthews1 <christopher.matthews@jbhunt.com>
2021-08-11 14:51:02 -0400

    challenge-5

--- scripts/modules/User.js
+++ scripts/modules/User.js
@@ -1,11 +1,11 @@
-function User(name, gender, age, registered, req, userStatus) {
+function User(name="Unregistered User", gender="unknown", age="unknown", registered=false, req="unknown", userStatus="0") {
     this.userName = name;
-    this.userGender = gender;
-    this.userAge = age;
-    this.userCode = this.userGender + this.userAge;
-    this.registered = false;
+    this.userGender = 9;
+    this.userAge = 8;
+    this.userCode = gender === "unknown" || age === "unknown" ? "unknown" : gender + age;
+    this.registered = registered;
     this.userReq = req;
-    this.userStatus = null;
+    this.userStatus = userStatus;
 }
 
 export { User }
